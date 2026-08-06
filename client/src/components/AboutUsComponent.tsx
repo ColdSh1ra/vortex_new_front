cfb1877cd7c4120e30ffd8ddce3e17ff56cff1ac
ColdSh1ra <a.v.res594@gmail.com>
2026-08-07 01:37:13 +0300

    About us block development pt.2

--- client/src/components/AboutUsComponent.tsx
+++ client/src/components/AboutUsComponent.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getHomepageContent } from '../services/api';
 import type { AboutUsContent } from '../types/content';
+import HeadingComponent from "./default/HeadingComponent";
+import DescriptionComponent from "./default/DescriptionComponent";
 // import BlueContainerBlock from "../components/default/BlueContainerBlock";
 // import TextContainer from "../components/default/TextContainer";
 // import ButtonFill from "../components/default/ButtonFIll";
@@ -26,21 +28,49 @@ function AboutUs() {
     }, []);
 
     return (
-        <section className="hero-page section-container">
+        <section className="about-us-content section-container">
             {isLoading && <p>Іде завантаження...</p>}
             {error && <p>Невдалось завантажити сторінку: {error}</p>}
             {content && (
-                <>
-                    <h2>{content.about_us_title}</h2>
-                    <p>{content.about_us_subtitle}</p>
+                <div className={'display-flex flex-column align-content-center'}>
+                    <HeadingComponent children={content.about_us_title}
+                                      adClassName={'about-us-hero-title text-color-orange'}
+                    />
+                    <DescriptionComponent children={content.about_us_subtitle}
+                                          adClassName={'about-us-hero-subtitle'}
+                    />
 
-                    {content.about_us_blocks.map((block) => (
-                        <div key={block.about_us_title}>
-                            <h3>{block.about_us_title}</h3>
-                            <p>{block.about_us_description}</p>
+                    {content.about_us_blocks.map((block, index) => (
+                        <div className={'about-us-block display-flex jc-space-between' +
+                            (index % 2 === 0 ? ' row-reverse ' : ' flex-direction-row ')}
+                             key={index}>
+                            <div className={'about-us-info-element display-flex flex-column'}
+                                 key={block.about_us_title + index}>
+                                <HeadingComponent
+                                    children={'/'+ (index + 1)}
+                                    adClassName={'about-us-index text-color-orange'}
+                                    />
+                                <HeadingComponent
+                                    children={block.about_us_title}
+                                    adClassName={'about-us-title text-color-white'}
+                                />
+                                <DescriptionComponent
+                                    children={block.about_us_description}
+                                    adClassName={'about-us-description text-color-white'}
+                                />
+                            </div>
+                            <div className={'display-flex align-content-center au-image-outer-wrapper'}>
+                                <div className={' '}>
+                                    <img src={block.about_us_image_path}
+                                         className={'radial-bg-container image'}
+                                         width="160"
+                                         height="160"
+                                         alt={'about us image'} />
+                                </div>
+                            </div>
                         </div>
                     ))}
-                </>
+                </div>
             )}
         </section>
     );
--- client/src/components/default/DescriptionComponent.tsx
+++ client/src/components/default/DescriptionComponent.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {JSX} from "react";
 
-interface DescriptionProps {
+type DescriptionProps =  {
     children: React.ReactNode,
     adClassName?: string
 }
 
-function Description({children, adClassName}: DescriptionProps) {
+function Description({children, adClassName}: DescriptionProps): JSX.Element {
     return <p className={"paragraph text-2xl font-bold " + adClassName}>{children}</p>
 }
 
